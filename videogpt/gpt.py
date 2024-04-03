1f3bea896a3922e18729599cd376245500bde4d4
Rushi Shah <shahrushi2003@gmail.com>
2024-04-03 14:49:06 +0530

    Added soft-discretization support

--- videogpt/gpt.py
+++ videogpt/gpt.py
@@ -9,11 +9,11 @@ import torch.nn.functional as F
 import torch.optim.lr_scheduler as lr_scheduler
 import pytorch_lightning as pl
 
-from .resnet import _, resnet18
+from .resnet import resnet18
 from .attention import AttentionStack, LayerNorm, AddBroadcastPosEmbed
 from .utils import shift_dim
 
-from torch_maskgit import MaskGit
+# from torch_maskgit import MaskGit
 
 
 class VideoGPT(pl.LightningModule):
@@ -24,10 +24,11 @@ class VideoGPT(pl.LightningModule):
         # Load VQ-VAE and set all parameters to no grad
         from .vqvae import VQVAE
         from .download import load_vqvae
+
         if not os.path.exists(args.vqvae):
             self.vqvae = load_vqvae(args.vqvae)
         else:
-            self.vqvae =  VQVAE.load_from_checkpoint(args.vqvae)
+            self.vqvae = VQVAE.load_from_checkpoint(args.vqvae)
         for p in self.vqvae.parameters():
             p.requires_grad = False
         self.vqvae.codebook._need_init = False
@@ -36,10 +37,12 @@ class VideoGPT(pl.LightningModule):
         # ResNet34 for frame conditioning
         self.use_frame_cond = args.n_cond_frames > 0
         if self.use_frame_cond:
-            frame_cond_shape = (args.n_cond_frames,
-                                args.resolution // 4,
-                                args.resolution // 4,
-                                192)
+            frame_cond_shape = (
+                args.n_cond_frames,
+                args.resolution // 4,
+                args.resolution // 4,
+                192,
+            )
             self.resnet = resnet18(1, (1, 4, 4), resnet_dim=192)
             self.cond_pos_embd = AddBroadcastPosEmbed(
                 shape=frame_cond_shape[:-1], embd_dim=frame_cond_shape[-1]
@@ -54,38 +57,45 @@ class VideoGPT(pl.LightningModule):
         self.fc_in.weight.data.normal_(std=0.02)
 
         self.attn_stack = AttentionStack(
-            self.shape, args.hidden_dim, args.heads, args.layers, args.dropout,
-            args.attn_type, args.attn_dropout, args.class_cond_dim, frame_cond_shape
+            self.shape,
+            args.hidden_dim,
+            args.heads,
+            args.layers,
+            args.dropout,
+            args.attn_type,
+            args.attn_dropout,
+            args.class_cond_dim,
+            frame_cond_shape,
         )
 
         self.norm = LayerNorm(args.hidden_dim, args.class_cond_dim)
 
-        # self.fc_out = nn.Linear(args.hidden_dim, self.vqvae.n_codes, bias=False)
-        # self.fc_out.weight.data.copy_(torch.zeros(self.vqvae.n_codes, args.hidden_dim))
+        self.fc_out = nn.Linear(args.hidden_dim, self.vqvae.n_codes, bias=False)
+        self.fc_out.weight.data.copy_(torch.zeros(self.vqvae.n_codes, args.hidden_dim))
 
-        MASKGIT_VOCAB_DIM = 256
-        MASKGIT_HIDDEN_DIM = int(192/2)
-        TFM_ARGS = {
-            "embed_dim": MASKGIT_HIDDEN_DIM,
-            "num_heads": 8,
-            "num_layers": 8,
-            "mlp_dim": MASKGIT_HIDDEN_DIM*4, # Can be anything
-            "dropout": 0.0,
-            "attention_dropout": 0.0,
+        # MASKGIT_VOCAB_DIM = 256
+        # MASKGIT_HIDDEN_DIM = int(192/2)
+        # TFM_ARGS = {
+        #     "embed_dim": MASKGIT_HIDDEN_DIM,
+        #     "num_heads": 8,
+        #     "num_layers": 8,
+        #     "mlp_dim": MASKGIT_HIDDEN_DIM*4, # Can be anything
+        #     "dropout": 0.0,
+        #     "attention_dropout": 0.0,
 
-            "vocab_dim": MASKGIT_VOCAB_DIM, # Required, else the code fails
-            # "vocab_size": vocab_size, # DONT USE INTERNAL TF EMBEDDINGS AS TECO DOESNT EITHE
-            "input_dim": args.hidden_dim # Input dimensions for pre-encoded tokens
-        }
+        #     "vocab_dim": MASKGIT_VOCAB_DIM, # Required, else the code fails
+        #     # "vocab_size": vocab_size, # DONT USE INTERNAL TF EMBEDDINGS AS TECO DOESNT EITHE
+        #     "input_dim": args.hidden_dim # Input dimensions for pre-encoded tokens
+        # }
 
-        # self.shape is (t, h, w) = (16, 4, 4) but we do per frame masking
-        maskgit_shape = self.shape[1:]
+        # # self.shape is (t, h, w) = (16, 4, 4) but we do per frame masking
+        # maskgit_shape = self.shape[1:]
 
-        self.maskgit = MaskGit(maskgit_shape,
-                               self.vqvae.n_codes,
-                               MASKGIT_VOCAB_DIM,
-                               'cosine',
-                               TFM_ARGS)
+        # self.maskgit = MaskGit(maskgit_shape,
+        #                        self.vqvae.n_codes,
+        #                        MASKGIT_VOCAB_DIM,
+        #                        'cosine',
+        #                        TFM_ARGS)
 
         # caches for faster decoding (if necessary)
         self.frame_cond_cache = None
@@ -101,13 +111,15 @@ class VideoGPT(pl.LightningModule):
         cond = dict()
         if self.use_frame_cond or self.args.class_cond:
             assert batch is not None
-            video = batch['video']
+            video = batch["video"]
 
             if self.args.class_cond:
-                label = batch['label']
-                cond['class_cond'] = F.one_hot(label, self.args.class_cond_dim).type_as(video)
+                label = batch["label"]
+                cond["class_cond"] = F.one_hot(label, self.args.class_cond_dim).type_as(
+                    video
+                )
             if self.use_frame_cond:
-                cond['frame_cond'] = video[:, :, :self.args.n_cond_frames]
+                cond["frame_cond"] = video[:, :, : self.args.n_cond_frames]
 
         samples = torch.zeros((n,) + self.shape).long().to(device)
         idxs = list(itertools.product(*[range(s) for s in self.shape]))
@@ -128,10 +140,15 @@ class VideoGPT(pl.LightningModule):
                     embeddings_slice = embeddings[prev_idx]
                     samples_slice = samples[prev_idx]
 
-                logits = self(embeddings_slice, samples_slice, cond,
-                              decode_step=i, decode_idx=idx)[1]
+                logits = self(
+                    embeddings_slice, samples_slice, cond, decode_step=i, decode_idx=idx
+                )[1]
                 # squeeze all possible dim except batch dimension
-                logits = logits.squeeze().unsqueeze(0) if logits.shape[0] == 1 else logits.squeeze()
+                logits = (
+                    logits.squeeze().unsqueeze(0)
+                    if logits.shape[0] == 1
+                    else logits.squeeze()
+                )
                 probs = F.softmax(logits, dim=-1)
                 samples[batch_idx] = torch.multinomial(probs, 1).squeeze(-1)
 
@@ -139,24 +156,26 @@ class VideoGPT(pl.LightningModule):
             samples = self.vqvae.decode(samples)
             samples = torch.clamp(samples, -0.5, 0.5) + 0.5
 
-        return samples # BCTHW in [0, 1]
+        return samples  # BCTHW in [0, 1]
 
     def forward_without_maskgit(self, x, cond, decode_step=None, decode_idx=None):
         if self.use_frame_cond:
             if decode_step is None:
-                cond['frame_cond'] = self.cond_pos_embd(self.resnet(cond['frame_cond']))
+                cond["frame_cond"] = self.cond_pos_embd(self.resnet(cond["frame_cond"]))
             elif decode_step == 0:
-                self.frame_cond_cache = self.cond_pos_embd(self.resnet(cond['frame_cond']))
-                cond['frame_cond'] = self.frame_cond_cache
+                self.frame_cond_cache = self.cond_pos_embd(
+                    self.resnet(cond["frame_cond"])
+                )
+                cond["frame_cond"] = self.frame_cond_cache
             else:
-                cond['frame_cond'] = self.frame_cond_cache
+                cond["frame_cond"] = self.frame_cond_cache
 
         h = self.fc_in(x)
         h = self.attn_stack(h, cond, decode_step, decode_idx)
         h = self.norm(h, cond)
 
         return h
-    
+
     def forward_maskgit(self, h, targets):
         # h is now of shape: (batch, t, h, w, c)
         dims = h.shape
@@ -171,13 +190,13 @@ class VideoGPT(pl.LightningModule):
         logits = logits.view(dims[0], dims[1], *logits.shape[1:])
 
         return logits
-    
+
     def forward(self, x, targets, cond, decode_step=None, decode_idx=None):
         h = self.forward_without_maskgit(x, cond, decode_step, decode_idx)
-        
-        # logits = self.fc_out(h)
 
-        logits = self.forward_maskgit(h, targets)
+        logits = self.fc_out(h)
+
+        # logits = self.forward_maskgit(h, targets)
 
         loss = F.cross_entropy(shift_dim(logits, -1, 1), targets)
 
@@ -185,14 +204,14 @@ class VideoGPT(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         self.vqvae.eval()
-        x = batch['video']
+        x = batch["video"]
 
         cond = dict()
         if self.args.class_cond:
-            label = batch['label']
-            cond['class_cond'] = F.one_hot(label, self.args.class_cond_dim).type_as(x)
+            label = batch["label"]
+            cond["class_cond"] = F.one_hot(label, self.args.class_cond_dim).type_as(x)
         if self.use_frame_cond:
-            cond['frame_cond'] = x[:, :, :self.args.n_cond_frames]
+            cond["frame_cond"] = x[:, :, : self.args.n_cond_frames]
 
         with torch.no_grad():
             targets, x = self.vqvae.encode(x, include_embeddings=True)
@@ -203,30 +222,36 @@ class VideoGPT(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         loss = self.training_step(batch, batch_idx)
-        self.log('val/loss', loss, prog_bar=True)
+        self.log("val/loss", loss, prog_bar=True)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=3e-4, betas=(0.9, 0.999))
-        assert hasattr(self.args, 'max_steps') and self.args.max_steps is not None, f"Must set max_steps argument"
+        assert (
+            hasattr(self.args, "max_steps") and self.args.max_steps is not None
+        ), f"Must set max_steps argument"
         scheduler = lr_scheduler.CosineAnnealingLR(optimizer, self.args.max_steps)
-        return [optimizer], [dict(scheduler=scheduler, interval='step', frequency=1)]
-
+        return [optimizer], [dict(scheduler=scheduler, interval="step", frequency=1)]
 
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument('--vqvae', type=str, default='kinetics_stride4x4x4',
-                            help='path to vqvae ckpt, or model name to download pretrained')
-        parser.add_argument('--n_cond_frames', type=int, default=0)
-        parser.add_argument('--class_cond', action='store_true')
+        parser.add_argument(
+            "--vqvae",
+            type=str,
+            default="kinetics_stride4x4x4",
+            help="path to vqvae ckpt, or model name to download pretrained",
+        )
+        parser.add_argument("--n_cond_frames", type=int, default=0)
+        parser.add_argument("--class_cond", action="store_true")
 
         # VideoGPT hyperparmeters
-        parser.add_argument('--hidden_dim', type=int, default=576)
-        parser.add_argument('--heads', type=int, default=4)
-        parser.add_argument('--layers', type=int, default=8)
-        parser.add_argument('--dropout', type=float, default=0.2)
-        parser.add_argument('--attn_type', type=str, default='full',
-                            choices=['full', 'sparse'])
-        parser.add_argument('--attn_dropout', type=float, default=0.3)
+        parser.add_argument("--hidden_dim", type=int, default=576)
+        parser.add_argument("--heads", type=int, default=4)
+        parser.add_argument("--layers", type=int, default=8)
+        parser.add_argument("--dropout", type=float, default=0.2)
+        parser.add_argument(
+            "--attn_type", type=str, default="full", choices=["full", "sparse"]
+        )
+        parser.add_argument("--attn_dropout", type=float, default=0.3)
 
         return parser
--- videogpt/vqvae.py
+++ videogpt/vqvae.py
@@ -11,6 +11,13 @@ import torch.distributed as dist
 from .attention import MultiHeadAttention
 from .utils import shift_dim
 
+import sys
+
+sys.path.insert(0, "/content/Soft-discretization")
+
+from semivq.nn import VectorQuant
+
+
 class VQVAE(pl.LightningModule):
     def __init__(self, args):
         super().__init__()
@@ -24,23 +31,29 @@ class VQVAE(pl.LightningModule):
         self.pre_vq_conv = SamePadConv3d(args.n_hiddens, args.embedding_dim, 1)
         self.post_vq_conv = SamePadConv3d(args.embedding_dim, args.n_hiddens, 1)
 
-        self.codebook = Codebook(args.n_codes, args.embedding_dim)
+        self.codebook = VectorQuant(
+            args.embedding_dim, args.n_codes, use_learnable_std=True
+        )
         self.save_hyperparameters()
 
     @property
     def latent_shape(self):
-        input_shape = (self.args.sequence_length, self.args.resolution,
-                       self.args.resolution)
-        return tuple([s // d for s, d in zip(input_shape,
-                                             self.args.downsample)])
+        input_shape = (
+            self.args.sequence_length,
+            self.args.resolution,
+            self.args.resolution,
+        )
+        return tuple([s // d for s, d in zip(input_shape, self.args.downsample)])
 
     def encode(self, x, include_embeddings=False):
         h = self.pre_vq_conv(self.encoder(x))
-        vq_output = self.codebook(h)
+        # print(h.shape)
+        zq, vq_dict = self.codebook(h)
+        vq_dict["q"] = vq_dict["q"].squeeze()
         if include_embeddings:
-            return vq_output['encodings'], vq_output['embeddings']
+            return vq_dict["q"], zq
         else:
-            return vq_output['encodings']
+            return vq_dict["q"]
 
     def decode(self, encodings):
         h = F.embedding(encodings, self.codebook.embeddings)
@@ -49,25 +62,27 @@ class VQVAE(pl.LightningModule):
 
     def forward(self, x):
         z = self.pre_vq_conv(self.encoder(x))
-        vq_output = self.codebook(z)
-        x_recon = self.decoder(self.post_vq_conv(vq_output['embeddings']))
+        # print(z.shape)
+        zq, vq_dict = self.codebook(z)
+        # print(vq_dict['q'].shape, zq.shape)
+        x_recon = self.decoder(self.post_vq_conv(zq))
         recon_loss = F.mse_loss(x_recon, x) / 0.06
 
-        return recon_loss, x_recon, vq_output
+        return recon_loss, x_recon, vq_dict
 
     def training_step(self, batch, batch_idx):
-        x = batch['video']
+        x = batch["video"]
         recon_loss, _, vq_output = self.forward(x)
-        commitment_loss = vq_output['commitment_loss']
+        commitment_loss = vq_output["loss"]
         loss = recon_loss + commitment_loss
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x = batch['video']
+        x = batch["video"]
         recon_loss, _, vq_output = self.forward(x)
-        self.log('val/recon_loss', recon_loss, prog_bar=True)
-        self.log('val/perplexity', vq_output['perplexity'], prog_bar=True)
-        self.log('val/commitment_loss', vq_output['commitment_loss'], prog_bar=True)
+        self.log("val/recon_loss", recon_loss, prog_bar=True)
+        self.log("val/perplexity", vq_output["perplexity"], prog_bar=True)
+        self.log("val/commitment_loss", vq_output["loss"], prog_bar=True)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=7e-4, betas=(0.9, 0.999))
@@ -75,26 +90,29 @@ class VQVAE(pl.LightningModule):
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument('--embedding_dim', type=int, default=256)
-        parser.add_argument('--n_codes', type=int, default=2048)
-        parser.add_argument('--n_hiddens', type=int, default=240)
-        parser.add_argument('--n_res_layers', type=int, default=4)
-        parser.add_argument('--downsample', nargs='+', type=int, default=(4, 4, 4))
+        parser.add_argument("--embedding_dim", type=int, default=256)
+        parser.add_argument("--n_codes", type=int, default=2048)
+        parser.add_argument("--n_hiddens", type=int, default=240)
+        parser.add_argument("--n_res_layers", type=int, default=4)
+        parser.add_argument("--downsample", nargs="+", type=int, default=(4, 4, 4))
         return parser
 
 
 class AxialBlock(nn.Module):
     def __init__(self, n_hiddens, n_head):
         super().__init__()
-        kwargs = dict(shape=(0,) * 3, dim_q=n_hiddens,
-                      dim_kv=n_hiddens, n_head=n_head,
-                      n_layer=1, causal=False, attn_type='axial')
-        self.attn_w = MultiHeadAttention(attn_kwargs=dict(axial_dim=-2),
-                                         **kwargs)
-        self.attn_h = MultiHeadAttention(attn_kwargs=dict(axial_dim=-3),
-                                         **kwargs)
-        self.attn_t = MultiHeadAttention(attn_kwargs=dict(axial_dim=-4),
-                                         **kwargs)
+        kwargs = dict(
+            shape=(0,) * 3,
+            dim_q=n_hiddens,
+            dim_kv=n_hiddens,
+            n_head=n_head,
+            n_layer=1,
+            causal=False,
+            attn_type="axial",
+        )
+        self.attn_w = MultiHeadAttention(attn_kwargs=dict(axial_dim=-2), **kwargs)
+        self.attn_h = MultiHeadAttention(attn_kwargs=dict(axial_dim=-3), **kwargs)
+        self.attn_t = MultiHeadAttention(attn_kwargs=dict(axial_dim=-4), **kwargs)
 
     def forward(self, x):
         x = shift_dim(x, 1, -1)
@@ -115,18 +133,19 @@ class AttentionResidualBlock(nn.Module):
             SamePadConv3d(n_hiddens // 2, n_hiddens, 1, bias=False),
             nn.BatchNorm3d(n_hiddens),
             nn.ReLU(),
-            AxialBlock(n_hiddens, 2)
+            AxialBlock(n_hiddens, 2),
         )
 
     def forward(self, x):
         return x + self.block(x)
 
+
 class Codebook(nn.Module):
     def __init__(self, n_codes, embedding_dim):
         super().__init__()
-        self.register_buffer('embeddings', torch.randn(n_codes, embedding_dim))
-        self.register_buffer('N', torch.zeros(n_codes))
-        self.register_buffer('z_avg', self.embeddings.data.clone())
+        self.register_buffer("embeddings", torch.randn(n_codes, embedding_dim))
+        self.register_buffer("N", torch.zeros(n_codes))
+        self.register_buffer("z_avg", self.embeddings.data.clone())
 
         self.n_codes = n_codes
         self.embedding_dim = embedding_dim
@@ -148,7 +167,7 @@ class Codebook(nn.Module):
         y = self._tile(flat_inputs)
 
         d = y.shape[0]
-        _k_rand = y[torch.randperm(y.shape[0])][:self.n_codes]
+        _k_rand = y[torch.randperm(y.shape[0])][: self.n_codes]
         if dist.is_initialized():
             dist.broadcast(_k_rand, 0)
         self.embeddings.data.copy_(_k_rand)
@@ -160,9 +179,11 @@ class Codebook(nn.Module):
         if self._need_init and self.training:
             self._init_embeddings(z)
         flat_inputs = shift_dim(z, 1, -1).flatten(end_dim=-2)
-        distances = (flat_inputs ** 2).sum(dim=1, keepdim=True) \
-                    - 2 * flat_inputs @ self.embeddings.t() \
-                    + (self.embeddings.t() ** 2).sum(dim=0, keepdim=True)
+        distances = (
+            (flat_inputs**2).sum(dim=1, keepdim=True)
+            - 2 * flat_inputs @ self.embeddings.t()
+            + (self.embeddings.t() ** 2).sum(dim=0, keepdim=True)
+        )
 
         encoding_indices = torch.argmin(distances, dim=1)
         encode_onehot = F.one_hot(encoding_indices, self.n_codes).type_as(flat_inputs)
@@ -190,7 +211,7 @@ class Codebook(nn.Module):
             self.embeddings.data.copy_(encode_normalized)
 
             y = self._tile(flat_inputs)
-            _k_rand = y[torch.randperm(y.shape[0])][:self.n_codes]
+            _k_rand = y[torch.randperm(y.shape[0])][: self.n_codes]
             if dist.is_initialized():
                 dist.broadcast(_k_rand, 0)
 
@@ -202,12 +223,17 @@ class Codebook(nn.Module):
         avg_probs = torch.mean(encode_onehot, dim=0)
         perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
 
-        return dict(embeddings=embeddings_st, encodings=encoding_indices,
-                    commitment_loss=commitment_loss, perplexity=perplexity)
+        return dict(
+            embeddings=embeddings_st,
+            encodings=encoding_indices,
+            commitment_loss=commitment_loss,
+            perplexity=perplexity,
+        )
 
     def dictionary_lookup(self, encodings):
         embeddings = F.embedding(encodings, self.embeddings)
         return embeddings
+
 
 class Encoder(nn.Module):
     def __init__(self, n_hiddens, n_res_layers, downsample):
@@ -224,8 +250,7 @@ class Encoder(nn.Module):
         self.conv_last = SamePadConv3d(in_channels, n_hiddens, kernel_size=3)
 
         self.res_stack = nn.Sequential(
-            *[AttentionResidualBlock(n_hiddens)
-              for _ in range(n_res_layers)],
+            *[AttentionResidualBlock(n_hiddens) for _ in range(n_res_layers)],
             nn.BatchNorm3d(n_hiddens),
             nn.ReLU()
         )
@@ -243,8 +268,7 @@ class Decoder(nn.Module):
     def __init__(self, n_hiddens, n_res_layers, upsample):
         super().__init__()
         self.res_stack = nn.Sequential(
-            *[AttentionResidualBlock(n_hiddens)
-              for _ in range(n_res_layers)],
+            *[AttentionResidualBlock(n_hiddens) for _ in range(n_res_layers)],
             nn.BatchNorm3d(n_hiddens),
             nn.ReLU()
         )
@@ -255,8 +279,7 @@ class Decoder(nn.Module):
         for i in range(max_us):
             out_channels = 3 if i == max_us - 1 else n_hiddens
             us = tuple([2 if d > 0 else 1 for d in n_times_upsample])
-            convt = SamePadConvTranspose3d(n_hiddens, out_channels, 4,
-                                           stride=us)
+            convt = SamePadConvTranspose3d(n_hiddens, out_channels, 4, stride=us)
             self.convts.append(convt)
             n_times_upsample -= 1
 
@@ -281,13 +304,14 @@ class SamePadConv3d(nn.Module):
         # assumes that the input shape is divisible by stride
         total_pad = tuple([k - s for k, s in zip(kernel_size, stride)])
         pad_input = []
-        for p in total_pad[::-1]: # reverse since F.pad starts from last dim
+        for p in total_pad[::-1]:  # reverse since F.pad starts from last dim
             pad_input.append((p // 2 + p % 2, p // 2))
         pad_input = sum(pad_input, tuple())
         self.pad_input = pad_input
 
-        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size,
-                              stride=stride, padding=0, bias=bias)
+        self.conv = nn.Conv3d(
+            in_channels, out_channels, kernel_size, stride=stride, padding=0, bias=bias
+        )
 
     def forward(self, x):
         return self.conv(F.pad(x, self.pad_input))
@@ -303,15 +327,19 @@ class SamePadConvTranspose3d(nn.Module):
 
         total_pad = tuple([k - s for k, s in zip(kernel_size, stride)])
         pad_input = []
-        for p in total_pad[::-1]: # reverse since F.pad starts from last dim
+        for p in total_pad[::-1]:  # reverse since F.pad starts from last dim
             pad_input.append((p // 2 + p % 2, p // 2))
         pad_input = sum(pad_input, tuple())
         self.pad_input = pad_input
 
-        self.convt = nn.ConvTranspose3d(in_channels, out_channels, kernel_size,
-                                        stride=stride, bias=bias,
-                                        padding=tuple([k - 1 for k in kernel_size]))
+        self.convt = nn.ConvTranspose3d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            bias=bias,
+            padding=tuple([k - 1 for k in kernel_size]),
+        )
 
     def forward(self, x):
         return self.convt(F.pad(x, self.pad_input))
-
